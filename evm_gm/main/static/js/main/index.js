ff5b0059495fd2393dc030a5a610b0de38300bb9
manhnd1112 <manhnd@kaopiz.com>
2018-02-25 09:30:17 +0700

    caculate eac(t) & some performance index

--- evm_gm/main/static/js/main/index.js
+++ evm_gm/main/static/js/main/index.js
@@ -10,33 +10,70 @@ $(document).ready(function(){
             }
         }
     });
+    
 });
 
 $('#btn-est-param').click(function(){
     var pd = $('#pd').val();
+    var budget = $('#bac').val();
     var evaluationPoint = $('#evaluation-point').val();
     console.log(evaluationPoint);
     var data = JSON.parse(dataStr);
     var useData = [];
-    useData[0] = data[0].slice(0, evaluationPoint);
-    useData[1] = data[1].slice(0, evaluationPoint);
-    useData[2] = data[2].slice(0, evaluationPoint);
-    useData[3] = data[3].slice(0, evaluationPoint); 
-     
+    useData[0] = data[0];
+    // PV
+    useData[1] = data[1];
+    // EV - PV
+    useData[2] = data[2].slice(0, evaluationPoint).concat(data[1].slice(evaluationPoint, pd));
+    // AC - PV
+    useData[3] = data[3].slice(0, evaluationPoint).concat(data[1].slice(evaluationPoint, pd));
+    console.log(useData[3])
     $.ajax({
         url: 'ajax/test',
         data: {
             'data': JSON.stringify(useData),
             'pd': pd,
-            'budget': data[1][data[1].length-1]
+            'budget': budget,
+            'evaluationPoint': evaluationPoint
         },
         method: 'GET',
         contentType: "application/json; charset=utf-8",
         success: function(data){
-            console.log(data.alpha);
+            console.log(data);
             $('#alpha').val(data.alpha);
             $('#beta').val(data.beta);
-            $('#gamma').val(data.gamma);            
+            $('#gamma').val(data.gamma);     
+            $('#ES').text(data.ES);
+            $('#SPI').text(data.SPI);       
+            $('#CPI').text(data.CPI);       
+            $('#SPIt').text(data.SPIt);       
+            $('#SCI').text(data.SCI);       
+            $('#SCIt').text(data.SCIt);       
+            $('#ED').text(data.ED);       
+            $('#EACtPV1').text(data.EACtPV1);
+            $('#EACtPV2').text(data.EACtPV2);
+            $('#EACtPV3').text(data.EACtPV3);
+            $('#EACtED1').text(data.EACtED1);            
+            $('#EACtED2').text(data.EACtED2);            
+            $('#EACtED3').text(data.EACtED3);
+            $('#EACtES1').text(data.EACtES1);                        
+            $('#EACtES2').text(data.EACtES2);                        
+            $('#EACtES3').text(data.EACtES3);                                    
         }
     })
+})
+
+$('#btn-test').click(function(){
+    var data = JSON.parse(dataStr);
+    var pd = $('#pd').val();
+    var budget = $('#bac').val();
+    var alpha = parseFloat($('#alpha').val());
+    var beta = parseFloat($('#beta').val());
+    var gamma = parseFloat($('#gamma').val());
+    var dataTest = parseInt($('#data-test').val());
+    //a * np.exp(-np.exp(b - c*xdata))
+    var resultTest = alpha * Math.exp(-Math.exp(beta - gamma*(data[0][dataTest-1]/pd)));
+    var rightResult = data[3][dataTest-1]/budget;
+    $('#result-test').text(resultTest+"");
+    $('#right-result').text(rightResult);
 })
--- evm_gm/main/views.py
+++ evm_gm/main/views.py
@@ -13,7 +13,6 @@ class Views:
         nrows = sheet.nrows - 1
         ncols = sheet.ncols
         data = []
-        print(nrows, ncols)
         for col in range(sheet.ncols):
             data.append(sheet.col_values(col))
         for i in range(len(data)):
@@ -21,10 +20,11 @@ class Views:
         return render(request, "main/index.html", {'data': data})
 
 class Ajax:
-    def init(X, Y , n, buggest, dataArr):
-        for i in range(len(dataArr)):
+    def init(AT, X, Y , n, buggest, AC_PV):
+        for i in range(n):
+            AT[i] = (i+1)*1.0
             X[i] = (i + 1)/(n*1.0)
-            Y[i] = dataArr[i]/(buggest*1.0)
+            Y[i] = AC_PV[i]/(buggest*1.0)
         
     # for gomperzt
     def exp_decay(parameters,xdata):
@@ -37,24 +37,121 @@ class Ajax:
     def residuals(parameters,x_data,y_observed,func):    
         return func(parameters,x_data) - y_observed
 
+    def getES(AT , PV, EV_PV, evaluationPoint):
+        # example: evaluationPoint = 9 -> current EV is: EV[8]. i = 0..8
+        EV = EV_PV[evaluationPoint-1]
+        t = [ n for n,i in enumerate(PV) if i>EV ][0] - 1
+        #print(PV[t_1], EV)
+        #print(t_1, t_1 - 1)
+        return round(AT[t] + (EV - PV[t])/(PV[t+1] - PV[t]), 2)
+    
+    def getSPI(PV, EV_PV, evaluationPoint):
+        return round(EV_PV[evaluationPoint -1]/PV[evaluationPoint - 1], 2) 
+
+    def getCPI(EV_PV, AC_PV, evaluationPoint):
+        return round(EV_PV[evaluationPoint -1]/AC_PV[evaluationPoint - 1], 2)
+
+    def getSPIt(AT , PV, EV_PV, evaluationPoint):
+        ES = Ajax.getES(AT , PV, EV_PV, evaluationPoint)
+        return round(ES/AT[evaluationPoint - 1], 2)
+    
+    def getSCI(SPI, CPI):
+        return round(SPI*CPI, 2)
+    
+    def getSCIt(SPIt, CPI):
+        return round(SPIt*CPI, 2)
+    
+    def getED(AT, SPI, evaluationPoint):
+        return round(AT[evaluationPoint -1]*SPI, 2)
+
+    def getSV(PV, EV_PV, evaluationPoint):
+        return EV_PV[evaluationPoint -1] - PV[evaluationPoint -1]
+
+    def getTV(BAC, PD, PV, EV_PV, evaluationPoint):
+        PVrate = BAC/PD
+        SV = Ajax.getSV(PV, EV_PV, evaluationPoint)
+        return round(SV/PVrate, 2)
+
+    def getEACtPV1(PD, TV):
+        return PD - TV
+    
+    def getEACtPV2(PD, SPI):
+        return round(PD/SPI, 2)
+
+    def getEACtPV3(PD, SCI):
+        return round(PD/SCI, 2)   
+
+    # return EACt caculated by ED
+    def getEACtED(PD, AT, ED, PF, evaluationPoint):
+        return round(AT[evaluationPoint -1] + (max(PD, AT[evaluationPoint -1]) - ED)/PF, 2)
+
+     # return EACt caculated by ES
+    def getEACtES(PD, AT, ES, PF, evaluationPoint):
+        return round(AT[evaluationPoint -1] + (PD - ES)/PF, 2)
+
     def test(request):
-        data = json.loads(request.GET.get('data'))
         pd = int(request.GET.get('pd'))
         budget = float(request.GET.get('budget'))
-        print(data, pd, budget)
-        xdata = np.zeros(len(data[3]))
-        ydata = np.zeros(len(data[3]))
-        Ajax.init(xdata, ydata, pd, budget, data[3])
-        x0 = [1, 1, 1] 
+        data = json.loads(request.GET.get('data'))
+        evaluationPoint = int(request.GET.get('evaluationPoint'))
+        PV = data[1]
+        EV_PV = data[2]
+        AC_PV = data[3]
+        AT = np.zeros(pd)
+        xdata = np.zeros(pd)
+        ydata = np.zeros(pd)
+
+        Ajax.init(AT, xdata, ydata, pd, budget, AC_PV)                
+        ES = Ajax.getES(AT, PV, EV_PV, evaluationPoint)
+        SPI = Ajax.getSPI(PV, EV_PV, evaluationPoint)
+        CPI = Ajax.getCPI(EV_PV, AC_PV, evaluationPoint)
+        SPIt = Ajax.getSPIt(AT, PV, EV_PV, evaluationPoint)
+        SCI = Ajax.getSCI(SPI, CPI)
+        SCIt = Ajax.getSCIt(SPIt, CPI)
+        ED = Ajax.getED(AT, SPI, evaluationPoint)
+
+        EACtPV1 = Ajax.getEACtPV1(pd, Ajax.getTV(budget, pd, PV, EV_PV, evaluationPoint))
+        EACtPV2 = Ajax.getEACtPV2(pd, SPI)
+        EACtPV3 = Ajax.getEACtPV3(pd, SCI)        
+
+        EACtED1 = Ajax.getEACtED(pd, AT, ED, 1, evaluationPoint)
+        EACtED2 = Ajax.getEACtED(pd, AT, ED, SPI, evaluationPoint)
+        EACtED3 = Ajax.getEACtED(pd, AT, ED, SCI, evaluationPoint)
+        
+        EACtES1 = Ajax.getEACtES(pd, AT, ES, 1, evaluationPoint)
+        EACtES2 = Ajax.getEACtES(pd, AT, ES, SPIt, evaluationPoint)
+        EACtES3 = Ajax.getEACtES(pd, AT, ES, SCIt, evaluationPoint)
+
+        print(ES, SPI, CPI, SPIt, SCI, SCIt, ED)
+
+        # print(xdata)
+        # print(ydata)
+        x0 = [0, 0, 0] 
         lb = [0,0,0]
         ub = [2,2,2]
-        OptimizeResult  = optimize.least_squares(Ajax.residuals,  x0, method = 'dogbox',
+        OptimizeResult  = optimize.least_squares(Ajax.residuals,  x0,method = 'dogbox',
                                           args   = ( xdata, ydata,Ajax.exp_decay) )
         parametersEstimated = OptimizeResult.x
         data = {
             'status': 'ok',
             'alpha': parametersEstimated[0],
             'beta': parametersEstimated[1],
-            'gamma': parametersEstimated[2]
+            'gamma': parametersEstimated[2],
+            'ES': ES,
+            'SPI': SPI,
+            'CPI': CPI, 
+            'SPIt': SPIt,
+            'SCI': SCI,
+            'SCIt': SCIt, 
+            'ED': ED,
+            'EACtPV1': EACtPV1,
+            'EACtPV2': EACtPV2,
+            'EACtPV3': EACtPV3,
+            'EACtED1': EACtED1,
+            'EACtED2': EACtED2,
+            'EACtED3': EACtED3,
+            'EACtES1': EACtES1,
+            'EACtES2': EACtES2,
+            'EACtES3': EACtES3
         }
         return JsonResponse(data)
